9bef7479c87ab0a176de6e084d45855bbdb0b32f
aleksndr <ostraleks@gmail.com>
2022-08-06 20:33:21 +0500

    7th

--- 2/Program.cs
+++ 2/Program.cs
@@ -3,11 +3,11 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22*/
-Console.Write("введите первое число");
+Console.Write("введите первое число ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите второе число");
+Console.Write("введите второе число ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-Console.Write("введите третье число");
+Console.Write("введите третье число ");
 int numberC = Convert.ToInt32(Console.ReadLine());
 int max = numberA;
 if (numberB > max)
